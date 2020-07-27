4a7119eb945f6836835ed68dd013c7a558c02a94
kimsungjin <tjdwls3469@naver.com>
2020-07-27 20:03:36 +0900

    [ISSUE-22] 10870번 피보나치 수 5  - 10870번 피보나치 수 5 풀이

---
+++ src/Baekjoon10870.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon10870 {
+	public static int[] memo = new int[21];
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(fibo(Integer.parseInt(br.readLine())));
+	}
+	
+	public static int fibo(int num) {
+		if(num <= 1) {
+			return  num;
+		}
+		if(memo[num] != 0) {
+			return memo[num];
+		}
+		return memo[num] = fibo(num-1) + fibo(num-2);
+	}
+}
