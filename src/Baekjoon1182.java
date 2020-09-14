7cbc338bd7b3e47775bc95ccbc82071bc6db7978
SungJin <tjdwls3469@naver.com>
2020-09-14 17:17:12 +0900

    [ISSUE-77] 백준 1182번 부분수열의 합  - 백쥰 1182번 부분수열의 합 풀이

---
+++ src/Baekjoon1182.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon1182 {
+	public static int N;
+	public static int S;
+	public static int[] ary;
+	public static int ans;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		ary = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			ary[i] = Integer.parseInt(st.nextToken());
+		}
+		generateSet(0, 0);
+		if(S == 0) {
+			ans--;
+		}
+		System.out.println(ans);
+	}
+	
+	public static void generateSet(int cnt, int sum) {
+		if(cnt == N) {
+			if(sum == S) {
+				ans++;
+			}
+			return;
+		}
+
+		generateSet(cnt + 1, sum + ary[cnt]);
+		generateSet(cnt + 1, sum);
+	}
+}
