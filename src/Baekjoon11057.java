e3a608cb2512f96810183f85986ddd9bde792fe4
SungJin <tjdwls3469@naver.com>
2020-10-25 13:51:15 +0900

    [ISSUE-107] 백준 11057번 오르막 수   - 백준 11057번 오르막 수 풀이

---
+++ src/Baekjoon11057.java
@@ -0,0 +1,51 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * d[n][k] : 길이가 n이고 마지막수가 k인 오르막 수의 개수
+ * 
+ * d[1][0] = 0
+ * d[1][1] = 1 
+ * d[1][2] = 1 
+ * d[1][3] = 1 
+ * d[1][4] = 1 
+ * d[1][5] = 1 
+ * d[1][6] = 1 
+ * d[1][7] = 1 
+ * d[1][8] = 1 
+ * d[1][9] = 1 
+ * 
+ * d[2][k] = d[1][k] ~ d[1][9]
+ */
+
+public class Baekjoon11057 {
+	private static final int MOD = 10007;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] memo = new int[1001][10];
+		
+		for (int i = 0; i <= 9; i++) {
+			memo[1][i] = 1;
+		}
+		
+		for (int i = 2; i <= N; i++) {
+			for (int j = 0; j <= 9; j++) {
+				for (int k = j; k <= 9; k++) {
+					memo[i][j] = (memo[i][j] + memo[i-1][k]) % MOD;
+				}
+			}
+		}
+		
+		int ans = 0;
+		for (int i = 0; i <= 9; i++) {
+			ans = (ans + memo[N][i]) % MOD;
+		}
+		
+		System.out.println(ans);
+	}
+}
