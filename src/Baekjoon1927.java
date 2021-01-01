93441d42a759fe05d0eedd45ed92381096e3979f
SungJin <tjdwls3469@naver.com>
2021-01-01 11:43:55 +0900

    [ISSUE-139] 백준 1927번 최소 힙 풀이   - 백준 1927번 최소 힙 풀이

---
+++ src/Baekjoon1927.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Baekjoon1927 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+		int x;
+		
+		for (int i = 0; i < N; i++) {
+			x = Integer.parseInt(br.readLine());
+			
+			if(x != 0) {
+				q.offer(x);
+				continue;
+			}
+			
+			if(q.isEmpty()) {
+				sb.append(0).append('\n');
+				continue;
+			}
+			
+			sb.append(q.poll()).append('\n');
+		}
+		
+		System.out.print(sb);
+	}
+}
