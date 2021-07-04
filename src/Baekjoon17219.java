5a0a2e4d73a10a4ccd26d845e63b53ce3847612f
Sungjin <tjdwls3469@naver.com>
2021-07-04 17:51:36 +0900

    [ISSUE-200] 백준 17219번 비밀번호 찾기 - 백준 17219번 비밀번호 찾기 풀이

---
+++ src/Baekjoon17219.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Baekjoon17219 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Map<String, String> map = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			map.put(st.nextToken(), st.nextToken());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			sb.append(map.get(st.nextToken())).append('\n');
+		}
+		
+		System.out.print(sb);
+	}
+}
