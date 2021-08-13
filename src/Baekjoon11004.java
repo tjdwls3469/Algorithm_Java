6c67c060d44ff7064c89251af94d7b51ff549c4d
Sungjin <tjdwls3469@naver.com>
2021-08-13 19:02:39 +0900

    [ISSUE-222] 백준 11004번 K번째 수 - 백준 11004번 K번째 수 풀이

---
+++ src/Baekjoon11004.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Baekjoon11004 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Collections.sort(list);
+		System.out.println(list.get(K-1));
+	}
+}
